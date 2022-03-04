72a777c255f80b8ccddd1e860c9829f4e62da11e
ccf-tfehlmann <tfehlmann@crohnscolitisfoundation.org>
2022-03-04 11:04:09 -0500

    Updated medication data to include infliximab biosimilars.

--- R/data.R
+++ R/data.R
@@ -34,13 +34,14 @@
 "header"
 
 
+
 #' med_grp
 #'
 #' A dataframe containing the coding for Medications. Allows to group the same
 #' medications together if they have different names (generic v. scientific)
-#' from different source.
+#' from different source. Infliximab Biosimilars added March 2022.
 #'
-#' @format A data frame with 90 rows and 3 variables:
+#' @format A data frame with 96 rows and 3 variables:
 #' \describe{
 #'   \item{MEDICATION_NAME}{Medication Name as it appears in SF, ECRF or EMR}
 #'   \item{med_type}{The class of medication}
