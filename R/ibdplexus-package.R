b1f584a333fb4b209785ed193ffcc3bd95112576
ccf-sharvey <sharvey@crohnscolitisfoundation.org>
2022-09-06 15:40:02 -0400

    Added function to extract certain diagnoses from EMR.
    Added DiagrammeR to imports.

--- R/ibdplexus-package.R
+++ R/ibdplexus-package.R
@@ -6,6 +6,7 @@
 #' @import openxlsx
 #' @import readxl
 #' @import reshape2
+#' @import DiagrammeR
 #' @importFrom tidyr drop_na pivot_wider
 #' @importFrom data.table fread
 #' @importFrom data.table rbindlist
