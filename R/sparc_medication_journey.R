bd903933586acd07ae8cbd7486dfa4803e5e1f34
Cass Bargell <cbargell@ccfa.org>
2025-08-08 15:34:48 -0400

    fix logic

--- R/sparc_medication_journey.R
+++ R/sparc_medication_journey.R
@@ -193,15 +193,15 @@ sparc_med_journey <- function(prescriptions, demographics, observations, encount
     ## drop med end date if either the most recent emr prescription date > 90
     ## days or the most recent emr encounter date > 90 days from the latest med
     ## end date we have for that medication
-    mutate(MED_END_DATE = if_else(is.na(MED_END_DATE_ECRF) & flag_pres90 == 1 & flag_enc90 == 1, MED_END_DATE, MED_END_DATE)) %>%
+    mutate(MED_END_DATE = if_else(is.na(MED_END_DATE_ECRF) & flag_pres90 == 1 & flag_enc90 == 1, NA, MED_END_DATE)) %>%
     ## add in logic using flags
     mutate(LOGIC_USED = case_when(
       !is.na(MED_END_DATE_ECRF) ~ "ECRF END DATE",
       is.na(MED_END_DATE_ECRF) & (flag_medver == 1 | flag_current == 1) ~ "MED VERIFICATION OR CURRENT FLAG OVERRIDE",
-      is.na(MED_END_DATE_ECRF) & (flag_pres90 == 1 | flag_enc90 == 1) ~ "EMR END DATE WITHIN 90 DAYS of EMR ENCOUNTER DATE",
+      is.na(MED_END_DATE_ECRF) & flag_pres90 == 1 & flag_enc90 == 1 ~ "EMR END DATE WITHIN 90 DAYS of EMR DATA DATE",
       is.na(MED_END_DATE_ECRF) & (!is.na(MED_END_DATE)) & !is.na(MED_END_DATE_EMR) ~ "EMR END DATE",
       is.na(MED_END_DATE_ECRF) & (!is.na(MED_END_DATE)) & is.na(MED_END_DATE_EMR) & MED_END_DATE == LAST_MED_START_EMR
-~ "LAST MED START DATE IN EMR",
+      ~ "LAST MED START DATE IN EMR",
 
       TRUE ~ "NO MED END DATES"
     )) %>%
